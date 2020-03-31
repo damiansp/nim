4fe5bb6bd36ac74bf0cad21e3dcd20b1591b23ea
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-03-30 19:22:09 -0700

    spawn msg in thread; add nims

--- in_action/03_chatApp/src/client.nim
+++ in_action/03_chatApp/src/client.nim
@@ -1,12 +1,15 @@
 #---------#---------#---------#---------#---------#---------#---------#---------
 import os
+import threadpool
 
 
 echo("Chat application started")
 if paramCount() == 0:
-    quit("Please specify the server address, e.g., ./client localhost")
+  quit("Please specify the server address, e.g., ./client localhost")
 
 let serverAddr = paramStr(1)
 echo("Connected to ", serverAddr)
-let message = stdin.readLine()
-echo("Sending \"", message, "\"")
+
+while true:
+  let message = spawn stdin.readLine()
+  echo("Sending \"", ^message, "\"")
