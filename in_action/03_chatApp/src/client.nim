baf2a8119f1f322a688a106d69fc86b31e479682
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-03-30 17:10:17 -0700

    add to client and del executable

--- in_action/03_chatApp/src/client.nim
+++ in_action/03_chatApp/src/client.nim
@@ -1,2 +1,12 @@
 #---------#---------#---------#---------#---------#---------#---------#---------
+import os
+
+
 echo("Chat application started")
+if paramCount() == 0:
+    quit("Please specify the server address, e.g., ./client localhost")
+
+let serverAddr = paramStr(1)
+echo("Connected to ", serverAddr)
+let message = stdin.readLine()
+echo("Sending \"", message, "\"")
