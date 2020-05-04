644b5ce2f2ebe747a8a08e05b18dddbc0910bd7a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-03 20:26:27 -0700

    tables++, init sets

--- in_action/04_stdlib/tables.nim
+++ in_action/04_stdlib/tables.nim
@@ -1,5 +1,24 @@
+import hashes, tables
+
 var numbers = @[3, 8, 1, 10]
 numbers.add(12)
 
 var animals = @["Dog", "Raccoon", "Sloth", "Cat"]
 animals.add("Red Panda")
+
+var animalAges = toTable[string, int]({"Dog": 3,
+																			 "Racoon": 8,
+																			 "Sloth": 1, 
+																			 "Cat": 10})
+animalAges["Red Panda"] = 12
+
+type
+	Dog = object
+		name: string
+
+proc hash(x: Dog): Hash =
+	result = x.name.hash
+	result = !$result # !$ operator checks for hash uniqueness
+
+var dogOwners = initTable[Dog, string]()
+dogOwners[Dog(name: "Koko")] = "Mom"
